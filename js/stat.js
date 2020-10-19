cdacf1af265bd854017351a1f2641e5a0d29a537
hqbalz <hqbalz@gmail.com>
2020-10-19 16:49:53 +0300

    Добавляет графики игроков

--- js/stat.js
+++ js/stat.js
@@ -1,8 +1,12 @@
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
-var TEXT_X = 100
+var TEXT_X = 100;
 var TEXT_Y = 10;
 var GAP_TEXT = 20;
+var GAP_BAR = 50;
+var BAR_WIDTH = 40;
+var BAR_HEIGHT = 150;
+var barHeight = BAR_HEIGHT;
 
 var textTitle = ['Ура вы победили!', 'Список результатов: '];
 
@@ -18,7 +22,7 @@ var renderText = function(ctx, text, x, y) {
   ctx.fillText(text, x, y);
 };
 
-window.renderStatistics = function(ctx) {
+window.renderStatistics = function(ctx, players) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, 100, 10, '#fff');
 
@@ -28,6 +32,29 @@ window.renderStatistics = function(ctx) {
       textTitle[i],
       TEXT_X + GAP_TEXT,
       TEXT_Y + GAP_TEXT * (i + 1)
+    );
+  }
+
+  for (var j = 0; j < players.length; j++) {
+    renderText(
+      ctx,
+      players[j],
+      TEXT_X + GAP_BAR + (GAP_BAR + BAR_WIDTH) * j,
+      TEXT_Y + GAP_TEXT * (textTitle.length + 1) + 5
+    );
+
+    ctx.fillRect(
+      TEXT_X + GAP_BAR + (GAP_BAR + BAR_WIDTH) * j,
+      TEXT_Y + GAP_TEXT * (textTitle.length + 1) + 5 + GAP_TEXT,
+      BAR_WIDTH,
+      barHeight
+    );
+
+    renderText(
+      ctx,
+      players[j],
+      TEXT_X + GAP_BAR + (GAP_BAR + BAR_WIDTH) * j,
+      TEXT_Y + GAP_TEXT * (textTitle.length + 1) + 15 + GAP_TEXT + BAR_HEIGHT
     );
   }
 };
