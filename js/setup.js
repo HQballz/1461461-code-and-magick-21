1dc67aab91f9294f30ecb196f954c7f61bd6d7cf
hqbalz <hqbalz@gmail.com>
2020-10-23 13:04:15 +0300

    Добавляет массивы с данными персонажей

---
+++ js/setup.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const WIZARD_NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+]
+
+const WIZARD_SURNAMES = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+]
+
+const WIZARD_COAT_COLORS = [
+  'rgb(241, 43, 107)',
+  'rgb(101, 137, 164)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+]
+
+const WIZARD_EYES_COLORS = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+]
+
+var setup = document.querySelector('.setup')
+
+setup.classList.remove('hidden')
